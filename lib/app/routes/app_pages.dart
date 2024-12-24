9ea2defe1da198b543b3d6aaa8be591822d43198
Rifai-13 <130067475+Rifai-13@users.noreply.github.com>
2024-12-24 12:11:10 +0700

    Review Film

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -32,7 +32,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.SplashScreen;
+  static const INITIAL = Routes.NAVBAR;
 
   static final routes = [
     GetPage(
